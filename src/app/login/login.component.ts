81ea834ccdc95ac59d26a01ac6fdad33fe19ead5
[gabriel.kho] <[gabriel.kho@pointwest.com.ph]>
2022-05-12 14:38:45 +0800

    edit validations of login and regstration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  registerForm = new FormGroup({
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)]), //min length 8 characters
+    
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    ])
+  });
 
-}
+    get email() {
+      return this.registerForm.get('email')
+    };
+
+    get password() {
+      return this.registerForm.get('password')
+    };
+  }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,48 +10,71 @@ export class RegistrationComponent {
 
   constructor() { }
 
+  maxDate = new Date();
   registerForm = new FormGroup({
     firstname: new FormControl('', Validators.required),
     lastname: new FormControl('', Validators.required),
+
+//      Example of valid email id
+//      mysite@ourearth.com
+//      my.ownsite@ourearth.org
+//      mysite@you.me.net
+//      Example of invalid email id
+//      mysite.ourearth.com [@ is not present]
+//      mysite@.com.my [ tld (Top Level domain) can not start with dot "." ]
+//      @you.me.net [ No character before @ ]
+//      mysite123@gmail.b [ ".b" is not a valid tld ]
+//      mysite@.org.org [ tld can not start with dot "." ]
+//      .mysite@mysite.org [ an email should not be start with "." ]
+//      mysite()*@gmail.com [ here the regular expression only allows character, digit, underscore, and dash ]
+//      mysite..1234@yahoo.com [double dots are not allowed]
+
     email: new FormControl('', [
       Validators.required,
       Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
     ]),
-    password: new FormControl('', Validators.required),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,20}$/)
+    ]),
+//      Max 20 characters, minimum six characters
+//      atleast one letter, one number and one special character
+    
     confirmpassword: new FormControl('', Validators.required),
     birthday: new FormControl('', Validators.required),
   });
 
   get firstName() {
-    return this.registerForm.get('firstname');
+    return this.registerForm.get('firstname')
   }
   
   get lastName() {
-    return this.registerForm.get('lastname');
+    return this.registerForm.get('lastname')
   }
   
   get email() {
-    return this.registerForm.get('email');
+    return this.registerForm.get('email')
   }
   
   get password() {
-    return this.registerForm.get('password');
+    return this.registerForm.get('password')
   }
   
   get confirmPassword() {
-    return this.registerForm.get('confirmpassword');
+    return this.registerForm.get('confirmpassword')
   }
   
   get birthday() {
-    return this.registerForm.get('birthday');
+    return this.registerForm.get('birthday')
   }
   
   get gender() {
-    return this.registerForm.get('gender');
+    return this.registerForm.get('gender')
   }
   
   get number() {
-    return this.registerForm.get('number');
+    return this.registerForm.get('number')
   }
 
   onSubmit() {
@@ -63,9 +86,18 @@ export class RegistrationComponent {
     {
       alert ("Passwords don't match");
     }
-  
-//add validation for birthday, birthday should not be later than today.
-//add validation for number, include country code
-
   }
+
+  //restricts user to input numbers only
+  keyPressNumbers(event: { which: any; keyCode: any; preventDefault: () => void; }) {
+    let charCode = (event.which) ? event.which : event.keyCode;
+    // Only Numbers 0-9
+    if ((charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
 }
