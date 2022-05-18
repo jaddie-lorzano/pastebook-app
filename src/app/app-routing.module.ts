4fdc7d7633b0526c60cdc61674f60e3c5268fb41
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-05-18 21:57:34 +0800

    Edit Album

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SignupPageComponent } from './components/signup-page/signup-page.component';
 import { FriendRequestPageComponent } from './components/friends-list-page/friend-request-page/friend-request-page.component';
-import { AlbumComponent } from './components/album-gallery-page/album/album.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 import { NewsFeedComponent } from './components/news-feed/news-feed.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { FriendsListPageComponent } from './components/friends-list-page/friends-list-page.component';
+import { AlbumGalleryPageComponent } from './components/album-gallery-page/album-gallery-page.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component:NewsFeedComponent},
   {path: 'login', pathMatch: 'full', component:LoginPageComponent},
   {path: 'signup', pathMatch: 'full', component:SignupPageComponent},
-  {path: 'albums', pathMatch: 'full', component:AlbumComponent},
+  {path: 'albums', pathMatch: 'full', component:AlbumGalleryPageComponent},
   {path: 'friendrequest', pathMatch: 'full', component:FriendRequestPageComponent},
   {path: 'profile', pathMatch: 'full', component:ProfilePageComponent},
   {path: 'friends', pathMatch: 'full', component:FriendsListPageComponent},
--- src/app/components/album-gallery-page/album-gallery-page.component.ts
+++ src/app/components/album-gallery-page/album-gallery-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Album } from 'src/app/models/Album';
+import { AlbumService } from 'src/app/services/album.service';
+import { CreateAlbumDialogComponent } from './create-album-dialog/create-album-dialog.component';
 
 @Component({
   selector: 'app-album-gallery-page',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumGalleryPageComponent implements OnInit {
 
-  constructor() { }
+  userAccountId = 111; // hard-coded for now
+  albums: Album[] = [];
+  constructor(private dialogRef:MatDialog, private albumService : AlbumService) { }
 
   ngOnInit(): void {
+    this.getAlbums();
   }
 
+  openDialog() {
+    this.dialogRef.open(CreateAlbumDialogComponent).afterClosed().subscribe(response => {
+      if(response == true) {
+        this.getAlbums()
+      }
+    })
+  }
+
+  getAlbums(): void {
+    this.albumService.getAlbums(this.userAccountId)
+      .subscribe(album => this.albums = album)
+  }
 }
