6e786a4b5075ae3ca18787d294c6ee6fabc00b61
[gabriel.kho] <[gabriel.kho@pointwest.com.ph]>
2022-05-19 00:46:42 +0800

    add validations and methods

--- src/app/components/settings-page/settings-page.component.ts
+++ src/app/components/settings-page/settings-page.component.ts
@@ -9,11 +9,18 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class SettingsPageComponent implements OnInit {
 
+  firstName:string = 'Cardo';
+  lastName:string = 'Dalisay';
+  birthday: any = '01/01/1990';
+  gender:string = 'Male';
+  mobileNumber: string = '096';
+  toggle: boolean = true;
+
   passwordForm = this.formBuilder.group({
     password: [null, [Validators.required,
-      Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*]{6,20}$/)]],
+      Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9!@#$%^&*_-]{6,20}$/)]],
     confirmpassword: [null, [Validators.required,
-      Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z0-9!@#$%^&*]{6,20}$/)]],
+      Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9!@#$%^&*_-]{6,20}$/)]],
     });
 
   constructor(private formBuilder : FormBuilder) { }
@@ -21,12 +28,21 @@ export class SettingsPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  checkPassword() {
-    // alert("going");
-    if (this.passwordForm.get('password')?.value === this.passwordForm.get('confirmpassword')?.value) {
-    alert ('Success');
-  } else {
-    alert ("Passwords don't match");
+  toggleInput() {
+    this.toggle = false;
   }
+
+  onClick() {
+    //save the variables to backend
+  }
+
+  checkPassword() {
+    if (this.passwordForm.get('password')?.value === this.passwordForm.get('confirmpassword')?.value) {
+    alert ('Your password has been changed');
+  } else {
+    alert ("Your password doesn't match");
+  }
+
+  //need method() if old passwords match. Need call from backend
 }
 }
