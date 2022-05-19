4e996e0fd124d943e3c3a6e1a90433b67a7954d8
[gabriel.kho] <[gabriel.kho@pointwest.com.ph]>
2022-05-19 10:47:14 +0800

    change gender from `other` to `prefer not to say`

--- src/app/components/settings-page/settings-page.component.ts
+++ src/app/components/settings-page/settings-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -9,12 +8,24 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class SettingsPageComponent implements OnInit {
 
+  accountSettingsForm = this.formBuilder.group({
+    firstname: [null, [Validators.required]],
+    lastName: [null, [Validators.required]],
+    birthDate: new FormControl('', Validators.required),
+    gender: null,
+    mobileNumber: null,
+  })
+
   firstName:string = 'Cardo';
   lastName:string = 'Dalisay';
-  birthday: any = '01/01/1990';
+  birthDate: any = '01/01/1990';
   gender:string = 'Male';
   mobileNumber: string = '096';
-  toggle: boolean = true;
+  firstNameInput: boolean = true;
+  lastNameInput: boolean = true;
+  birthDateInput: boolean = true;
+  genderInput: boolean = true;
+  numberInput: boolean = true;
 
   passwordForm = this.formBuilder.group({
     password: [null, [Validators.required,
@@ -28,21 +39,46 @@ export class SettingsPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  toggleInput() {
-    this.toggle = false;
+  toggleFirstName() {
+    this.firstNameInput = false;
+  }
+  toggleLastName() {
+    this.lastNameInput = false;
+  }
+  toggleBirthday() {
+    this.birthDateInput = false;
+  }
+  toggleGender() {
+    this.genderInput = false;
+  }
+  toggleNumber() {
+    this.numberInput = false;
   }
 
   onClick() {
-    //save the variables to backend
+    if(this.firstName == null || this.lastName == null || this.birthDate == null)
+    {
+
+    }
   }
 
   checkPassword() {
     if (this.passwordForm.get('password')?.value === this.passwordForm.get('confirmpassword')?.value) {
     alert ('Your password has been changed');
-  } else {
-    alert ("Your password doesn't match");
+    } else {
+      alert ("Your password doesn't match");
+    }
   }
-
   //need method() if old passwords match. Need call from backend
-}
+
+  keyPressNumbers(event: { which: any; keyCode: any; preventDefault: () => void; }) {
+    let charCode = (event.which) ? event.which : event.keyCode;
+    // Only Numbers 0-9
+    if ((charCode < 48 || charCode > 57)) {
+      event.preventDefault();
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
