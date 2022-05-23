aff65cf74641925454ea7ea5a460eccb79511a0c
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-05-23 09:04:10 +0800

    remove error matcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { CustomErrorStateMatcher } from './custom-state-matcher';
 import { AuthService } from './services/auth.service';
 
 @Component({
